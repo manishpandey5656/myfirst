92cead52a32c4e18136f7dd7f9943db079ab1ba8
Manish <157600211+manishpandey5656@users.noreply.github.com>
2024-10-23 20:56:05 +0530

    Create palindromeChecker.java

---
+++ palindromeChecker.java
@@ -0,0 +1,25 @@
+public class PalindromChecker {
+	
+	int j;
+	// defining the method to check either the string is pallindrome or not
+		public void palindromeChecker(String  word) {
+			System.out.println(word);
+			for( int i = 0, j =word.length() -1 ; i<j ;i++ ,j--) { // here we used the condition with two variables , if they satify only than String will be pallindrome
+				if(word.toLowerCase().charAt(i) == word.toLowerCase().charAt(j)) { 
+					System.out.println(word +" is pallindrome");
+					return;
+				}
+				else {
+					System.out.println(word+ " is not pallindrome");
+					return;
+				}
+			}
+				}
+		 public static void main(String args[]) {
+			 PalindromChecker p1 = new PalindromChecker();
+			 p1.palindromeChecker("mada");
+			  p1.palindromeChecker("mADAM");
+		 }
+		}
+		 
+		 
