19ba67423771c2154dc587d1341fc9b92b54b3d7
Manish <157600211+manishpandey5656@users.noreply.github.com>
2024-10-23 21:25:14 +0530

    Update palindromeChecker.java

--- palindromeChecker.java
+++ palindromeChecker.java
@@ -1,25 +1,55 @@
 // question:-
  //Check for Palindrome : Write a program to check if a word is a palindrome (e.g., 121).
 
- public class PalindromChecker {
+ // public class PalindromChecker {
 	
-	int j;
-	// defining the method to check either the string is pallindrome or not
-		public boolean palindromeChecker(String  word) {
-			System.out.println(word);
-			for( int i = 0, j =word.length() -1 ; i<j ;i++ ,j--) { // here we used the condition with two variables , if they satify only than String will be pallindrome
-				if(word.toLowerCase().charAt(i) == word.toLowerCase().charAt(j)) { 
-					 return true;
+	// int j;
+	// // defining the method to check either the string is pallindrome or not
+	// 	public boolean palindromeChecker(String  word) {
+	// 		System.out.println(word);
+	// 		for( int i = 0, j =word.length() -1 ; i<j ;i++ ,j--) { // here we used the condition with two variables , if they satify only than String will be pallindrome
+	// 			if(word.toLowerCase().charAt(i) == word.toLowerCase().charAt(j)) { 
+	// 				 return true;
 					
-				}
+	// 			}
 				  
-			}
-			return false;
-				}
-		 public static void main(String args[]) {
-			 PalindromChecker p1 = new PalindromChecker();
-			 System.out.println(p1.palindromeChecker("mada"));
-			  System.out.println(p1.palindromeChecker("mADAM"));
-		 }
-		}
+	// 		}
+	// 		return false;
+	// 			}
+	// 	 public static void main(String args[]) {
+	// 		 PalindromChecker p1 = new PalindromChecker();
+	// 		 System.out.println(p1.palindromeChecker("mada"));
+	// 		  System.out.println(p1.palindromeChecker("mADAM"));
+	// 	 }
+	// 	}
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+         
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        // Reverse the given number.
+        while (number != 0) {
+            int digit = number % 10;  
+            reversedNumber = reversedNumber * 10 + digit;  
+            number = number / 10;  
+        }
+
+      
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+    }
+         
+}
+
 		 
